83d1143a1a47113264cf42340aa1d15292f09934
anooppius-bbcommerce <127185335+anooppius-bbcommerce@users.noreply.github.com>
2023-03-19 14:24:50 +0530

    Update next.config.js

--- apps/saleor-app-checkout/next.config.js
+++ apps/saleor-app-checkout/next.config.js
@@ -10,7 +10,7 @@ const checkoutEmbededInStorefrontPath = "/saleor-app-checkout";
 
 /** @type {import('next').NextConfig} */
 const config = withTM({
-  target: “serverless”,
+  target: "serverless",
   trailingSlash: true,
   i18n: {
     locales: ["en-US", "pl-PL", "fr-FR", "vi-VN", "ar-AE"],
